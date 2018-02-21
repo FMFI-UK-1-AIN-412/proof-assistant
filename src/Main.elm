a6ab5fc0b0fcfcec9bbdf9176fc4f029ffff1ebd
Zoltan Onody <zoltan.onody@gmail.com>
2018-02-22 00:11:43 +0100

    Serious zipper rewrite

--- src/Main.elm
+++ src/Main.elm
@@ -4,328 +4,128 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 import Json.Decode as Decode
-
-
-type alias Element =
-    { value : String }
-
-
-type Tree
-    = Leaf Element
-    | Alfa Element Tree
-    | Beta Element (List Tree)
-
-
-type Breadcrumb
-    = BreadcrumbAlfa Element
-    | BreadcrumbBeta Element (List Tree) (List Tree)
-
-
-type alias Zipper =
-    { tree : Tree, breadcrumbs : List Breadcrumb }
+import Zipper
 
 
 type alias Model =
-    { zipper : Zipper }
+    { zipper : Zipper.Zipper }
 
 
 type Msg
-    = EditZipper Zipper String
-    | RemoveChildren Zipper
-    | AddChild Zipper Tree
-
-
-editTreeValue : Tree -> String -> Tree
-editTreeValue tree value =
-    case tree of
-        Leaf elem ->
-            Leaf { elem | value = value }
-
-        Alfa elem tree ->
-            Alfa { elem | value = value } tree
-
-        Beta elem trees ->
-            Beta { elem | value = value } trees
-
-
-getElemFromTree : Tree -> Element
-getElemFromTree tree =
-    case tree of
-        Leaf elem ->
-            elem
-
-        Alfa elem _ ->
-            elem
-
-        Beta elem _ ->
-            elem
-
-
-
-------------------------------------------
-
-
-goUp : Zipper -> Maybe Zipper
-goUp zipper =
-    let
-        rest =
-            List.drop 1 zipper.breadcrumbs
-    in
-        List.head zipper.breadcrumbs
-            |> Maybe.map
-                (\breadcrumb ->
-                    case breadcrumb of
-                        BreadcrumbAlfa element ->
-                            { tree = Alfa element zipper.tree
-                            , breadcrumbs = rest
-                            }
-
-                        BreadcrumbBeta element leftTrees rightTrees ->
-                            { tree = Beta element (leftTrees ++ [ zipper.tree ] ++ rightTrees)
-                            , breadcrumbs = rest
-                            }
-                )
-
-
-goRoot : Zipper -> Zipper
-goRoot zipper =
-    case goUp zipper of
-        Just newZipper ->
-            goRoot newZipper
-
-        Nothing ->
-            zipper
-
-
-goUpOrNothing : Zipper -> Zipper
-goUpOrNothing zipper =
-    Maybe.withDefault zipper (goUp zipper)
-
-
-goDown : Zipper -> Int -> Maybe Zipper
-goDown zipper index =
-    case zipper.tree of
-        Leaf _ ->
-            Nothing
-
-        Alfa elem tree ->
-            if index == 0 then
-                Just
-                    { tree = tree
-                    , breadcrumbs = (BreadcrumbAlfa elem) :: zipper.breadcrumbs
-                    }
-            else
-                Nothing
-
-        Beta elem trees ->
-            let
-                maybeTree =
-                    List.head (List.drop index trees)
-
-                leftTrees =
-                    List.take index trees
-
-                rightTrees =
-                    List.drop (index + 1) trees
-
-                breadCrumb =
-                    BreadcrumbBeta elem leftTrees rightTrees
-            in
-                case maybeTree of
-                    Just tree ->
-                        Just
-                            { tree = tree
-                            , breadcrumbs = breadCrumb :: zipper.breadcrumbs
-                            }
-
-                    Nothing ->
-                        Nothing
-
-
-goDownOrNothing : Zipper -> Int -> Zipper
-goDownOrNothing zipper index =
-    Maybe.withDefault zipper (goDown zipper index)
-
-
-removeChildren : Zipper -> Zipper
-removeChildren zipper =
-    case zipper.tree of
-        Leaf _ ->
-            zipper
-
-        Alfa elem _ ->
-            { zipper | tree = Leaf elem }
-
-        Beta elem _ ->
-            { zipper | tree = Leaf elem }
-
-
-addChild : Zipper -> Tree -> Zipper
-addChild zipper tree =
-    zipper
-
-
-
-------------------------------------------
+    = EditZipper Zipper.Zipper String
 
 
 initialModel : Model
 initialModel =
     { zipper =
-        { tree =
-            Alfa
-                { value = "ako" }
-                (Beta
-                    { value = "sa" }
-                    [ (Beta { value = "mas" }
-                        [ Leaf { value = "Dada?" }
-                        , Leaf { value = "Viki?" }
-                        , Beta { value = "Alexandra?" }
-                            [ Leaf { value = "Fine!" }
-                            , Leaf { value = "Zle!" }
-                            ]
-                        , Alfa
-                            { value = "Afla 1" }
-                            (Alfa
-                                { value = "Afla 2" }
-                                (Alfa
-                                    { value = "Afla 3" }
-                                    (Leaf
-                                        { value = "Leaft!" }
-                                    )
-                                )
-                            )
-                        ]
-                      )
-                    , (Leaf { value = "dari" })
-                    ]
-                )
-        , breadcrumbs = []
-        }
+        Zipper.Empty
+            |> Zipper.add (Zipper.createElement "p -> q")
+            |> Zipper.add (Zipper.createElement "q <-> r")
+            |> Zipper.goDown 0
+            |> Zipper.add (Zipper.createElement "q -> r")
+            |> Zipper.goDown 0
+            |> Zipper.add (Zipper.createElement "r -> q")
+            |> Zipper.goDown 0
+            |> Zipper.add (Zipper.createElement "p -> r")
+            |> Zipper.add (Zipper.createElement "p")
+            |> Zipper.goDown 1
+            |> Zipper.add (Zipper.createElement "q")
+            |> Zipper.goDown 0
+            |> Zipper.add (Zipper.createElement "r")
+            |> Zipper.goRoot
     }
 
 
-showSimpleTree : Tree -> String
-showSimpleTree tree =
-    case tree of
-        Leaf element ->
-            element.value
+showSimpleTree : Zipper.Zipper -> String
+showSimpleTree zipper =
+    let
+        data =
+            case Zipper.getValue zipper of
+                Just ans ->
+                    ans
 
-        Alfa element subtree ->
-            "(" ++ element.value ++ "->" ++ (showSimpleTree subtree) ++ ")"
+                Nothing ->
+                    ""
 
-        Beta element subtrees ->
-            "(" ++ element.value ++ "->(" ++ (String.join "|" (List.map showSimpleTree subtrees)) ++ "))"
+        showChildren =
+            String.join "|" <| List.map showSimpleTree (Zipper.getChildren zipper)
+    in
+        if showChildren == "" then
+            data
+        else
+            "(" ++ data ++ "->" ++ showChildren ++ ")"
 
 
-showEditableTree : Zipper -> Html Msg
+showEditableTree : Zipper.Zipper -> Html Msg
 showEditableTree zipper =
     div
         []
         [ h2 [] [ text "Edit the tabloid bellow" ]
-        , showEditableTree2 zipper []
+        , ul [] (showEditableTree2 zipper)
         ]
 
 
-
---showEditableTree2 : Zipper ->  Html Msg
-
-
-showEditableTree2 zipper styles =
+showEditableTree2 : Zipper.Zipper -> List (Html Msg)
+showEditableTree2 zipper =
     let
-        inpt =
-            div []
-                [ input
-                    [ onInput (EditZipper zipper)
-                    , value (getElemFromTree zipper.tree).value
-                    ]
-                    []
-                , button [ onClick (RemoveChildren zipper) ] [ text "x" ]
-                , button
-                    [ onClick (AddChild zipper (Alfa { value = "" } zipper.tree))
-                    ]
-                    [ text "A" ]
-                , button [ onClick (RemoveChildren zipper) ] [ text "B" ]
-                ]
+        value_text =
+            case Zipper.getValue zipper of
+                Just x ->
+                    x
 
-        base_style =
-            [ ( "padding-top", "5px" )
+                Nothing ->
+                    "ERROR!!!"
 
-            --, ( "padding-left", "5px" )
-            ]
+        mainElement =
+            li [] [ input [ onInput <| EditZipper zipper, value value_text ] [] ]
 
-        no_margin =
-            [ ( "margin-left", "0" ) ] ++ base_style
-
-        some_margin =
-            [ ( "border-left", "1px dashed #cfcfcf" )
-            , ( "margin-left", "20px" )
-            ]
-                ++ base_style
+        all =
+            List.map
+                showEditableTree2
+                (Zipper.getChildren zipper)
 
         rest =
-            case zipper.tree of
-                Leaf _ ->
-                    []
+            List.foldr (++) [] (List.drop 1 all)
 
-                Alfa _ subtree ->
-                    case goDown zipper 0 of
-                        Nothing ->
-                            []
-
-                        Just sub ->
-                            [ showEditableTree2 sub no_margin ]
-
-                Beta _ subtrees ->
-                    let
-                        range =
-                            List.range 0 (List.length subtrees - 1)
-
-                        maybeSubs =
-                            List.map (\i -> goDown zipper i) range
-
-                        inputs =
-                            List.map
-                                (\maybeSub ->
-                                    case maybeSub of
-                                        Nothing ->
-                                            div [] []
-
-                                        Just sub ->
-                                            showEditableTree2 sub some_margin
-                                )
-                                maybeSubs
-                    in
-                        inputs
+        maybeFirst =
+            List.head all
     in
-        div [ style styles ] (inpt :: rest)
+        case maybeFirst of
+            Nothing ->
+                [ mainElement, ul [] rest ]
+
+            Just first ->
+                [ mainElement, ul [] rest ] ++ first
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "bakalarka" ]
-        , p [] [ text (showSimpleTree (goRoot model.zipper).tree) ]
+        [ h1 [] [ text "Proof assistant" ]
+        , p [] [ text (showSimpleTree <| Zipper.goRoot model.zipper) ]
         , hr [] []
-        , showEditableTree (goRoot model.zipper)
+        , showEditableTree <| Zipper.goRoot model.zipper
         ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    let
-        newZipper =
-            case msg of
-                EditZipper zipper str ->
-                    { zipper | tree = editTreeValue zipper.tree str }
+    -- todo: dont ignore the messages
+    ( model, Cmd.none )
 
-                RemoveChildren zipper ->
-                    removeChildren zipper
 
-                AddChild zipper tree ->
-                    addChild zipper tree
-    in
-        ( { model | zipper = newZipper }, Cmd.none )
+
+--let
+--    newZipper =
+--        case msg of
+--            EditZipper zipper str ->
+--                { zipper | tree = Zipper.editTreeValue zipper.tree str }
+--            RemoveChildren zipper ->
+--                Zipper.removeChildren zipper
+--            AddChild zipper tree ->
+--                Zipper.addChild zipper tree
+--in
+--    ( { model | zipper = newZipper }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
---
+++ src/Zipper.elm
@@ -0,0 +1,228 @@
+module Zipper exposing (Zipper(..), add, getValue, createElement, goDown, goUp, goRoot, getChildren)
+
+
+type alias Element =
+    { value : String }
+
+
+createElement : String -> Element
+createElement string =
+    { value = string }
+
+
+type Tree
+    = Leaf Element
+    | Alfa Element Tree
+    | Beta Element (List Tree)
+
+
+getElementFromTree : Tree -> Element
+getElementFromTree tree =
+    case tree of
+        Leaf elem ->
+            elem
+
+        Alfa elem _ ->
+            elem
+
+        Beta elem _ ->
+            elem
+
+
+addToTree : Tree -> Element -> Tree
+addToTree tree element =
+    case tree of
+        Leaf old_element ->
+            Alfa old_element (Leaf element)
+
+        Alfa old_element old_tree ->
+            Beta old_element [ old_tree, Leaf element ]
+
+        Beta old_element old_trees ->
+            Beta old_element (old_trees ++ [ Leaf element ])
+
+
+type Breadcrumb
+    = BreadcrumbAlfa Element
+    | BreadcrumbBeta Element (List Tree) (List Tree)
+
+
+type Zipper
+    = Empty
+    | Zipper ZipperData
+
+
+type alias ZipperData =
+    { tree : Tree, breadcrumbs : List Breadcrumb }
+
+
+add : Element -> Zipper -> Zipper
+add element zipper =
+    case zipper of
+        Empty ->
+            Zipper <| ZipperData (Leaf element) []
+
+        Zipper previousData ->
+            Zipper <| ZipperData (addToTree previousData.tree element) previousData.breadcrumbs
+
+
+getValue : Zipper -> Maybe String
+getValue zipper =
+    case zipper of
+        Empty ->
+            Nothing
+
+        Zipper data ->
+            Just <| (getElementFromTree data.tree).value
+
+
+
+--editTreeValue : Tree -> String -> Tree
+--editTreeValue tree value =
+--    case tree of
+--        Leaf elem ->
+--            Leaf { elem | value = value }
+--        Alfa elem tree ->
+--            Alfa { elem | value = value } tree
+--        Beta elem trees ->
+--            Beta { elem | value = value } trees
+
+
+childrenCount : Zipper -> Int
+childrenCount zipper =
+    case zipper of
+        Empty ->
+            0
+
+        Zipper data ->
+            case data.tree of
+                Leaf _ ->
+                    0
+
+                Alfa _ _ ->
+                    1
+
+                Beta _ children ->
+                    List.length children
+
+
+getChildren : Zipper -> List Zipper
+getChildren zipper =
+    let
+        range =
+            List.range 0 <| childrenCount zipper - 1
+    in
+        List.map (\i -> goDown i zipper) range
+
+
+goUp : Zipper -> Zipper
+goUp zipper =
+    case zipper of
+        Empty ->
+            Empty
+
+        Zipper data ->
+            let
+                rest =
+                    List.drop 1 data.breadcrumbs
+            in
+                case List.head data.breadcrumbs of
+                    Nothing ->
+                        Empty
+
+                    Just breadcrumb ->
+                        case breadcrumb of
+                            BreadcrumbAlfa element ->
+                                Zipper
+                                    { tree = Alfa element data.tree
+                                    , breadcrumbs = rest
+                                    }
+
+                            BreadcrumbBeta element leftTrees rightTrees ->
+                                Zipper
+                                    { tree = Beta element (leftTrees ++ [ data.tree ] ++ rightTrees)
+                                    , breadcrumbs = rest
+                                    }
+
+
+goRoot : Zipper -> Zipper
+goRoot zipper =
+    case goUp zipper of
+        Zipper parent ->
+            goRoot <| Zipper parent
+
+        Empty ->
+            zipper
+
+
+
+--goUpOrStop : Zipper -> Zipper
+--goUpOrStop zipper =
+--    Maybe.withDefault zipper (goUp zipper)
+
+
+goDown : Int -> Zipper -> Zipper
+goDown index zipper =
+    case zipper of
+        Empty ->
+            Empty
+
+        Zipper data ->
+            case data.tree of
+                Leaf _ ->
+                    Empty
+
+                Alfa elem tree ->
+                    if index == 0 then
+                        Zipper
+                            { tree = tree
+                            , breadcrumbs = (BreadcrumbAlfa elem) :: data.breadcrumbs
+                            }
+                    else
+                        Empty
+
+                Beta elem trees ->
+                    let
+                        maybeTree =
+                            List.head (List.drop index trees)
+
+                        leftTrees =
+                            List.take index trees
+
+                        rightTrees =
+                            List.drop (index + 1) trees
+
+                        breadCrumb =
+                            BreadcrumbBeta elem leftTrees rightTrees
+                    in
+                        case maybeTree of
+                            Just tree ->
+                                Zipper
+                                    { tree = tree
+                                    , breadcrumbs = breadCrumb :: data.breadcrumbs
+                                    }
+
+                            Nothing ->
+                                Empty
+
+
+
+--goDownOrStop : Zipper -> Int -> Zipper
+--goDownOrStop zipper index =
+--    Maybe.withDefault zipper (goDown zipper index)
+--removeChildren : Zipper -> Zipper
+--removeChildren zipper =
+--    case zipper.tree of
+--        Leaf _ ->
+--            zipper
+--        Alfa elem _ ->
+--            { zipper | tree = Leaf elem }
+--        Beta elem _ ->
+--            { zipper | tree = Leaf elem }
+{- todo:
+   empty: Zipper
+   add: zipper -> tree -> zipper
+   remove: zipper -> zipper
+
+
+-}
