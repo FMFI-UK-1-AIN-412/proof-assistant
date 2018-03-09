7951c459da43713a10b73fdc689a1397a94dcdc9
Zoltan Onody <zoltan.onody@gmail.com>
2018-03-09 13:12:02 +0100

    review branch

--- src/Matcher.elm
+++
@@ -1,8 +0,0 @@
-module Matcher exposing (isOk)
-
-import Formula
-
-
-isOk : Formula.Formula -> List Formula.Formula -> Maybe String
-isOk formula formulas =
-    Just "This is not a result of the formulas above."
